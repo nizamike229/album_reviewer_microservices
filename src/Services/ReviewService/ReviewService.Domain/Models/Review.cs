1781a87e48128c90f177f6f0211dab7b98296fc9
nizamike229 <nizamike229@mail.ru>
2025-07-17 12:35:59 +0500

    added Regex

--- src/Services/ReviewService/ReviewService.Domain/Models/Review.cs
+++ src/Services/ReviewService/ReviewService.Domain/Models/Review.cs
@@ -1,4 +1,6 @@
-﻿namespace ReviewService.Domain.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ReviewService.Domain.Models;
 
 public class Review
 {
@@ -8,6 +10,8 @@ public class Review
 
     public required string TrackRatings { get; set; }
 
+    [MinLength(300)]
+    [RegularExpression(@"^(?!\s*$).+", ErrorMessage = "The field cannot consist only of spaces")]
     public required string Description { get; set; }
 
     public required string UserId { get; set; }
