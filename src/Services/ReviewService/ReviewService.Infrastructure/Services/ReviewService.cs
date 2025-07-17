a50dc7b84184c97873f6f27bc46877d72e474d6b
nizamike229 <nizamike229@mail.ru>
2025-07-17 22:32:52 +0500

    Implemented IReviewService with CRUD methods

---
+++ src/Services/ReviewService/ReviewService.Infrastructure/Services/ReviewService.cs
@@ -0,0 +1,35 @@
+using ReviewService.Application.Services;
+using ReviewService.Domain.Models;
+using ReviewService.Domain.Repositories;
+
+namespace ReviewService.Infrastructure.Services;
+
+public class ReviewService : IReviewService
+{
+    private readonly IReviewRepository _reviewRepository;
+
+    public ReviewService(IReviewRepository reviewRepository)
+    {
+        _reviewRepository = reviewRepository;
+    }
+
+    public Task<string> Post(Review review)
+    {
+        return _reviewRepository.Post(review);
+    }
+
+    public Task<List<Review>> GetAll(string mbId)
+    {
+        return _reviewRepository.GetAll(mbId);
+    }
+
+    public Task<AverageReviewModel> GetAverage(string mbId)
+    {
+        return _reviewRepository.GetAverage(mbId);
+    }
+
+    public Task<List<Review>> GetPersonal(string userId)
+    {
+        return _reviewRepository.GetPersonal(userId);
+    }
+}
