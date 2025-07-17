62a91fce9f410ab8e92a69981d7411c027c4df63
nizamike229 <nizamike229@mail.ru>
2025-07-17 22:49:30 +0500

    Enabled authentication middleware in the application pipeline

--- src/Services/ReviewService/ReviewService.Web/Program.cs
+++ src/Services/ReviewService/ReviewService.Web/Program.cs
@@ -22,6 +22,7 @@ if (app.Environment.IsDevelopment())
 app.UseMiddleware<ExceptionHandlerMiddleware>();
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
