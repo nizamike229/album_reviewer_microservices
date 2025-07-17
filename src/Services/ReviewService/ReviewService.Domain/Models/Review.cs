09a0a37f4cc0dd37ebb4f978ae6b476c94ca31a7
nizamike229 <nizamike229@mail.ru>
2025-07-17 11:42:58 +0500

    Scaffolded table to entities

---
+++ src/Services/ReviewService/ReviewService.Domain/Models/Review.cs
@@ -0,0 +1,16 @@
+﻿namespace ReviewService.Domain.Models;
+
+public class Review
+{
+    public required string Id { get; set; }
+
+    public required double? Rating { get; set; }
+
+    public required string TrackRatings { get; set; }
+
+    public required string Description { get; set; }
+
+    public required string UserId { get; set; }
+
+    public required string MbId { get; set; }
+}
---
+++ src/Services/ReviewService/ReviewService.Infrastructure/Context/ReviewDbContext.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ReviewService.Domain.Models;
+
+namespace ReviewService.Infrastructure.Context;
+
+public partial class ReviewDbContext : DbContext
+{
+    public ReviewDbContext()
+    {
+    }
+
+    public ReviewDbContext(DbContextOptions<ReviewDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Review> Reviews { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlite("Data Source=../main.sqlite");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Review>(entity =>
+        {
+            entity.ToTable("review");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.Description)
+                .HasColumnType("text(2000)")
+                .HasColumnName("description");
+            entity.Property(e => e.MbId).HasColumnName("mb_id");
+            entity.Property(e => e.Rating).HasColumnName("rating");
+            entity.Property(e => e.TrackRatings).HasColumnName("track_ratings");
+            entity.Property(e => e.UserId).HasColumnName("user_id");
+        });
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
