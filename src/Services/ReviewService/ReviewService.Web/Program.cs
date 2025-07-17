ddfae7df6a4a137b479e9880ab5ddb12fb72693e
nizamike229 <nizamike229@mail.ru>
2025-07-17 11:54:51 +0500

    Registered extension method

--- src/Services/ReviewService/ReviewService.Web/Program.cs
+++ src/Services/ReviewService/ReviewService.Web/Program.cs
@@ -1,8 +1,11 @@
+using ReviewService.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 
 builder.Services.AddControllers();
 builder.Services.AddSwaggerGen();
+builder.Services.AddServices();
 
 var app = builder.Build();
 
