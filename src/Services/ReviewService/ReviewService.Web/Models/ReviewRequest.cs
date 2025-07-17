7606899e09219f6080d2b87dfb5589211ee47be7
nizamike229 <nizamike229@mail.ru>
2025-07-17 22:49:45 +0500

    Removed `UserId` property from `ReviewRequest` model to streamline input data structure

--- src/Services/ReviewService/ReviewService.Web/Models/ReviewRequest.cs
+++ src/Services/ReviewService/ReviewService.Web/Models/ReviewRequest.cs
@@ -11,7 +11,5 @@ public class ReviewRequest
     [MinLength(300)]
     [RegularExpression(@"^(?!\s*$).+", ErrorMessage = "The field cannot consist only of spaces")]
     public required string Description { get; set; }
-
-    public required string UserId { get; set; }
     public required string MbId { get; set; }
 }
