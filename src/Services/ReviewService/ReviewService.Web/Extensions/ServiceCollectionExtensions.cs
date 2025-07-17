05d032a42dbd750a3a2db6c24869a32d3b50c4f6
nizamike229 <nizamike229@mail.ru>
2025-07-17 22:33:26 +0500

    Added `AddJwtAuthentication` extension method to configure JWT authentication and authorization

--- src/Services/ReviewService/ReviewService.Web/Extensions/ServiceCollectionExtensions.cs
+++ src/Services/ReviewService/ReviewService.Web/Extensions/ServiceCollectionExtensions.cs
@@ -1,3 +1,6 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 using ReviewService.Domain.Repositories;
 using ReviewService.Infrastructure.Repositories;
 
@@ -5,6 +8,48 @@ namespace ReviewService.Extensions;
 
 public static class ServiceCollectionExtensions
 {
+    public static void AddJwtAuthentication(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = builder.Configuration["Jwt:Issuer"],
+                    ValidAudience = builder.Configuration["Jwt:Audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(
+                        Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]!))
+                };
+
+                options.Events = new JwtBearerEvents
+                {
+                    OnMessageReceived = context =>
+                    {
+                        var authHeader = context.Request.Headers["Authorization"].FirstOrDefault();
+                        if (!string.IsNullOrEmpty(authHeader) && authHeader.StartsWith("Bearer "))
+                        {
+                            context.Token = authHeader.Substring("Bearer ".Length).Trim();
+                        }
+                        else if (context.Request.Cookies.ContainsKey("access_token"))
+                        {
+                            context.Token = context.Request.Cookies["access_token"];
+                        }
+
+                        return Task.CompletedTask;
+                    }
+                };
+            });
+        builder.Services.AddAuthorization();
+    }
+
     public static void AddServices(this IServiceCollection services)
     {
         services.AddScoped<IReviewRepository, ReviewRepository>();
