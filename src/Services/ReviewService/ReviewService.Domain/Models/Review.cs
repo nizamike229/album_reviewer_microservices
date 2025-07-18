49683ba12407ac3bfb996d1b012858b670861865
nizamike229 <nizamike229@mail.ru>
2025-07-18 13:50:41 +0500

    Removed validation attributes from `Description` property in `Review` model.

--- src/Services/ReviewService/ReviewService.Domain/Models/Review.cs
+++ src/Services/ReviewService/ReviewService.Domain/Models/Review.cs
@@ -1,19 +1,11 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace ReviewService.Domain.Models;
+﻿namespace ReviewService.Domain.Models;
 
 public class Review
 {
     public required string Id { get; set; }
-
     public required double? Rating { get; set; }
-
     public required string TrackRatings { get; set; }
-
-    [MinLength(300)]
-    [RegularExpression(@"^(?!\s*$).+", ErrorMessage = "The field cannot consist only of spaces")]
     public required string Description { get; set; }
-
     public required string UserId { get; set; }
     public required string MbId { get; set; }
 }
