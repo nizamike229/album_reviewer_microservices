abeeb0fdacb2f03eac0201e82379fe1be56b48d6
nizamike229 <nizamike229@mail.ru>
2025-07-18 13:50:46 +0500

    Updated `IReviewService.GetAll` method to return `ReviewResponse` instead of `Review` for consistent response structure.

--- src/Services/ReviewService/ReviewService.Application/Services/IReviewService.cs
+++ src/Services/ReviewService/ReviewService.Application/Services/IReviewService.cs
@@ -1,11 +1,12 @@
 using ReviewService.Domain.Models;
+using ReviewService.Domain.Models.Response;
 
 namespace ReviewService.Application.Services;
 
 public interface IReviewService
 {
     Task<string> Post(Review review);
-    Task<List<Review>> GetAll(string mbId);
+    Task<List<ReviewResponse>> GetAll(string mbId);
     Task<AverageReviewModel> GetAverage(string mbId);
     Task<List<Review>> GetPersonal(string userId);
 }
