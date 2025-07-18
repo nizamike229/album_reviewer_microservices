2ec5ebbe69eb9b65caa435b34e7e8745f96e9a42
nizamike229 <nizamike229@mail.ru>
2025-07-18 16:53:04 +0500

    Added `apiClient` utility for managing API requests and authentication.

---
+++ src/presentation/src/lib/apiClient.js
@@ -0,0 +1,60 @@
+import axios from "axios";
+
+let accessToken = "";
+let currentUsername = "";
+
+/**
+ * @param {string} token
+ */
+export function setAccessToken(token) {
+  accessToken = token;
+  document.cookie = `access_token=${token}; path=/;`;
+}
+
+export function getAccessToken() {
+  const match = document.cookie.match(/(?:^|; )access_token=([^;]*)/);
+  return match ? match[1] : null;
+}
+
+/**
+ * @param {string} username
+ */
+export function setUsername(username) {
+  currentUsername = username;
+
+  document.cookie = `username=${username}; path=/;`;
+}
+
+export function getUsername() {
+  if (typeof document === "undefined") return null;
+  const match = document.cookie.match(/(?:^|; )username=([^;]*)/);
+  return match ? match[1] : null;
+}
+
+const apiClient = axios.create({
+  baseURL: "https://localhost:7154",
+  withCredentials: true,
+  headers: {
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${getAccessToken() || ""}`,
+  },
+});
+
+apiClient.interceptors.request.use((config) => {
+  if (accessToken) {
+    config.headers["Authorization"] = `Bearer ${accessToken}`;
+  }
+  return config;
+});
+
+apiClient.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      window.location.href = "/promo";
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default apiClient;
